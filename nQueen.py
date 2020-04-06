552a4cf5a14899b546e594a8c01d1a56d69c3043
rahilansari <rahil@roaringstudios.in>
2020-04-06 20:50:23 +0530

    third commit

--- nQueen.py
+++ nQueen.py
@@ -1,25 +1,19 @@
 # Python program to solve N Queen 
 # Problem using backtracking 
 
-global N 
+# global N
 N = 4
 
 def printSolution(board):    
 	for i in range(N): 
 		for j in range(N): 
-			print (board[i][j])
+			print (board[i][j], end =" ")
 		print()
 
 
-# A utility function to check if a queen can 
-# be placed on board[row][col]. Note that this 
-# function is called when "col" queens are 
-# already placed in columns from 0 to col -1. 
-# So we need to check only left side for 
-# attacking queens 
 def isSafe(board, row, col): 
-
-	# Check this row on left side 
+	
+	# Check this row on left side 	
 	for i in range(col): 
 		if board[row][i] == 1: 
 			return False
@@ -37,55 +31,49 @@ def isSafe(board, row, col):
 	return True
 
 def solveNQUtil(board, col): 
-	# base case: If all queens are placed 
-	# then return true 
+
 	if col >= N: 
 		return True
+	
 
-	# Consider this column and try placing 
-	# this queen in all rows one by one 
 	for i in range(N): 
 
 		if isSafe(board, i, col): 
-			# Place this queen in board[i][col] 
-			board[i][col] = 1
 
-			# recur to place rest of the queens 
+			board[i][col] = 1			
+
 			if solveNQUtil(board, col + 1) == True: 
 				return True
 
-			# If placing queen in board[i][col 
-			# doesn't lead to a solution, then 
-			# queen from board[i][col] 
 			board[i][col] = 0
-
-	# if the queen can not be placed in any row in 
-	# this colum col then return false 
+	# print(board)
 	return False
 
-# This function solves the N Queen problem using 
-# Backtracking. It mainly uses solveNQUtil() to 
-# solve the problem. It returns false if queens 
-# cannot be placed, otherwise return true and 
-# placement of queens in the form of 1s. 
-# note that there may be more than one 
-# solutions, this function prints one of the 
-# feasible solutions. 
-def solveNQ(): 
-	board = [ [0, 0, 0, 0], 
-			[0, 0, 0, 0], 
-			[0, 0, 0, 0], 
-			[0, 0, 0, 0] 
-			] 
+
+def solveNQ():
+	global N
+	
+	N = int(input())
+	r, c = (N, N) 
+	board = [[0]*c]*r
+
+	# board = [ [0, 0, 0, 0], 
+    #           [0, 0, 0, 0], 
+    #           [0, 0, 0, 0], 
+    #           [0, 0, 0, 0] 
+    #         ] 
+  
+
 
 	if solveNQUtil(board, 0) == False: 
-		print ("Solution does not exist")
+		print ("NO")
 		return False
 
+	print("YES")
 	printSolution(board) 
 	return True
 
-# driver program to test above function 
+
 solveNQ() 
 
-# This code is contributed by Divyanshu Mehta 
+
